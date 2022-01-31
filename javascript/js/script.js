e9ab324190366c3b8d1fece5a8b5a95b7ce066ad
Luan <ipsluan@icloud.com>
2022-01-31 14:58:55 -0300

    Transformar String em Number como função

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -1,4 +1,7 @@
 var numero = "23";
-console.log(numero);
 
-console.log(+numero); // Transformar a String em Number - apenas se houver números na String
+function transformNumber(string) {
+  return Number(string);
+}
+
+console.log(transformNumber(numero));

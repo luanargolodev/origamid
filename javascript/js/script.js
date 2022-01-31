ba2d297df373413b297e3ff699a28d081878c108
Luan <ipsluan@icloud.com>
2022-01-31 16:21:12 -0300

    Operadores Lógicos ||

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -1,12 +1,13 @@
-// Operadores Lógicos &&
+// Operadores Lógicos ||
 
-true && true; // true
-true && false; // false
-false && true; // false
-"Gato" && "Cão"; // 'Cão'
-5 - 5 && 5 + 5; // 0
-"Gato" && false; // false
-5 >= 5 && 3 < 6; // true
+// || Compara se uma expressão ou outra é verdadeira
 
-// Se ambos os valores forem true, ele irá retornar o último valor verificado
-// Se algum valor for false, ele irá retornar o mesmo e não irá continuar a verificar os próximos
+true || true; // true
+true || false; // true
+false || true; // true
+"Gato" || "Cão"; // Gato
+5 - 5 || 5 + 5; // 10
+"Gato" || false; // Gato
+5 >= 5 || 3 < 6; // true
+
+// Retorna o primeiro valor true que encontrar

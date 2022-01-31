c706c7fa28e03d0ed3ee785e1950abe39de47077
Luan <ipsluan@icloud.com>
2022-01-31 14:16:47 -0300

    Conceito de incrementar um número

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -1,9 +1,4 @@
-// Ordem matemática = multiplicação, divisão, soma e por final substração.
-var total1 = 20 + 5 * 2;
-console.log(total1);
+var numero = 5;
+console.log(numero++);
 
-// Porém, é possível 'forçar' a resolver logo uma operação matemática antes de todo o conjunto, assim como na matemática
-total1 = (20 + 5) * 2;
-console.log(total1);
-
-// Usa-se parênteses para priorizar uma expressão
+console.log(numero);

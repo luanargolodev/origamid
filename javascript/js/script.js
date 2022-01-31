872a62a63df4e87412ad8b4cc746842766436a45
Luan <ipsluan@icloud.com>
2022-01-31 16:09:53 -0300

    Operadores Lógicos &&

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -1,12 +1,12 @@
-// Operadores de comparação
+// Operadores Lógicos &&
 
-// O == faz uma comparação não tão estrita
-// O === faz uma comparação estrita, ou seja, o tipo de dado deve ser o mesmo quando usamos ===
+true && true; // true
+true && false; // false
+false && true; // false
+"Gato" && "Cão"; // 'Cão'
+5 - 5 && 5 + 5; // 0
+"Gato" && false; // false
+5 >= 5 && 3 < 6; // true
 
-10 == "10"; // true
-10 == 10; // true
-10 === "10"; // false
-10 === 10; // true
-10 != 15; // true
-10 != "10"; // false
-10 !== "10"; // true
+// Se ambos os valores forem true, ele irá retornar o último valor verificado
+// Se algum valor for false, ele irá retornar o mesmo e não irá continuar a verificar os próximos

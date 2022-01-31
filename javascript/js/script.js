73ca05dfebc295713525163989470d0f71849668
Luan <ipsluan@icloud.com>
2022-01-31 15:10:21 -0300

    Entendendo como declarar booleans

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -1,7 +1,14 @@
-var numero = "23";
+var possuiGraduacao = false;
+var possuiDoutorado = false;
 
-function transformNumber(string) {
-  return Number(string);
+if (possuiGraduacao) {
+  console.log("Possui graudação.");
+} else {
+  console.log("Não possui graduação.");
 }
 
-console.log(transformNumber(numero));
+if (possuiDoutorado) {
+  console.log("Possui doutorado.");
+} else {
+  console.log("Não possui doutorado.");
+}

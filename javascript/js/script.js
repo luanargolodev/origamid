a1b788c8fe8b912156840d618f19cf0be6b3eb61
Luan <ipsluan@icloud.com>
2022-01-31 16:04:07 -0300

    Operadores de comparação

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -1,7 +1,12 @@
 // Operadores de comparação
 
-10 > 5; // true
-5 > 10; // false
-20 < 10; // false
-10 <= 10; // true
-10 >= 11; // false
+// O == faz uma comparação não tão estrita
+// O === faz uma comparação estrita, ou seja, o tipo de dado deve ser o mesmo quando usamos ===
+
+10 == "10"; // true
+10 == 10; // true
+10 === "10"; // false
+10 === 10; // true
+10 != 15; // true
+10 != "10"; // false
+10 !== "10"; // true

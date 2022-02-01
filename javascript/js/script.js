dba90bb01e042cf97c18ffa549e371ef1eb4140b
Luan <ipsluan@icloud.com>
2022-01-31 22:45:16 -0300

    Exercícios para resolver

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -1,19 +1,28 @@
-// Escopo Léxico
+// Crie uma função para verificar se um valor é Truthy
 
-// Funções conseguem acessar variáveis que foram criadas no contexto pai
+// Crie uma função matemática que retorne o perímetro de um quadrado
+// lembrando: perímetro é a soma dos quatro lados do quadrado
 
-var profissao = "Programador";
+// Crie uma função que retorne o seu nome completo
+// ela deve possuir os parâmetros: nome e sobrenome
 
-function dados() {
-  var nome = "Luan";
-  var idade = 23;
-  function outroDados() {
-    var endereco = "Ilhéus";
-    var idade = 23;
-    return `${nome}, ${idade}, ${endereco}, ${profissao}`;
-  }
-  return outroDados();
+// Crie uma função que verifica se um número é par
+
+// Crie uma função que retorne o tipo de
+// dado do argumento passado nela (typeof)
+
+// addEventListener é uma função nativa do JavaScript
+// o primeiro parâmetro é o evento que ocorre e o segundo o Callback
+// utilize essa função para mostrar no console o seu nome completo
+// quando o evento 'scroll' ocorrer.
+
+// Corrija o erro abaixo
+function precisoVisitar(paisesVisitados) {
+  var totalPaises = 193;
+  return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
 }
-
-console.log(dados()); // Retorna 'Luan, 23, Ilhéus, Programador
-// outroDados(); // Retorna um erro
+function jaVisitei(paisesVisitados) {
+  return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
+}
+precisoVisitar(20);
+jaVisitei(20);

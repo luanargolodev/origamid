b00aba00f1b0fc9cf515708549a230fb1a9b2f2b
Luan <ipsluan@icloud.com>
2022-01-31 23:38:46 -0300

    Resolução do exercício

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -1,20 +1,42 @@
 // Crie uma função para verificar se um valor é Truthy
+function isTruthy(dado) {
+  return !!dado;
+}
 
 // Crie uma função matemática que retorne o perímetro de um quadrado
 // lembrando: perímetro é a soma dos quatro lados do quadrado
+function perimetroQuadrado(lado) {
+  return lado * 4;
+}
 
 // Crie uma função que retorne o seu nome completo
 // ela deve possuir os parâmetros: nome e sobrenome
+function nomeCompleto(nome, sobrenome) {
+  return `${nome} ${sobrenome} `;
+}
 
 // Crie uma função que verifica se um número é par
+function isEven(numero) {
+  if (numero % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 // Crie uma função que retorne o tipo de
 // dado do argumento passado nela (typeof)
+function tipoDeDado(dado) {
+  return typeof dado;
+}
 
 // addEventListener é uma função nativa do JavaScript
 // o primeiro parâmetro é o evento que ocorre e o segundo o Callback
 // utilize essa função para mostrar no console o seu nome completo
 // quando o evento 'scroll' ocorrer.
+addEventListener("scroll", function () {
+  console.log("Luan Argolo");
+});
 
 // Corrija o erro abaixo
 var totalPaises = 193;

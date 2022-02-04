81131ad089de42c40a1e26760557f288efae5ed7
Luan Argolo <ipsluan@icloud.com>
2022-02-04 20:03:15 -0300

    Tudo é Objeto

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -1,46 +1,10 @@
-// Protótipo e Herança
-// O objeto herda propriedades e métodos do objeto que foi utilizado para criar o mesmo.
+// Tudo é Objeto
 
-var menu = {
-  width: 800,
-};
+// Strings, Números, Boolean, Objetos e mais, possuem propriedades e métodos. Por isso são objetos.
 
-console.log(menu.hasOwnProperty("width"));
-console.log(menu.hasOwnProperty("height"));
+var nome = "Luan";
 
-// hasOwnProperty é um método de Object
-
-var dados = {
-  nome: "Luan",
-  sobrenome: "Argolo",
-  idade: 23,
-  cidade: "Ilhéus",
-};
-
-// Crie um método no objeto anterior, que mostre o seu nome completo
-dados.nomeCompleto = function () {
-  return `${this.nome} ${this.sobrenome}`;
-};
-
-// Modifique o valor da propriedade preco para 3000
-var carro = {
-  preco: 1000,
-  portas: 4,
-  marca: "Audi",
-};
-
-carro.preco = 3000;
-
-// Crie um objeto de um cachorro que represente um labrador preto com 10 anos, que late ao ver um homem
-var cachorro = {
-  raca: "Labrador",
-  cor: "preto",
-  idade: 10,
-  latir(pessoa) {
-    if (pessoa == "homem") {
-      return "Latir";
-    } else {
-      return "Nada";
-    }
-  },
-};
+nome.length; // 4
+nome.charAt(1); // u
+nome.replace("an", "am"); // 'Luam'
+nome; // Luan

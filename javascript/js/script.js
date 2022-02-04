d3ee59cebffe7e291dd0e48cf7adecc3d557e17b
Luan Argolo <ipsluan@icloud.com>
2022-02-04 15:32:02 -0300

    Protótipo e Herança

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -1,14 +1,11 @@
-// Palavra-chave this
-// this irá fazer uma referência ao próprio objeto
+// Protótipo e Herança
+// O objeto herda propriedades e métodos do objeto que foi utilizado para criar o mesmo.
 
-var height = 120;
 var menu = {
   width: 800,
-  height: 50,
-  metadeHeight() {
-    return this.height / 2;
-  },
 };
 
-console.log(menu.metadeHeight());
-// sem o this, seria 60
+console.log(menu.hasOwnProperty("width"));
+console.log(menu.hasOwnProperty("height"));
+
+// hasOwnProperty é um método de Object

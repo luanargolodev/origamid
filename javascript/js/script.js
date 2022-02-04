ebc68de2fa6ff8b1f4448ba7f2490713a504afda
Luan Argolo <ipsluan@icloud.com>
2022-02-04 15:08:21 -0300

    Objetos

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -1,50 +1,12 @@
-// Crie uma função para verificar se um valor é Truthy
-function isTruthy(dado) {
-  return !!dado;
-}
+// Objetos
 
-// Crie uma função matemática que retorne o perímetro de um quadrado
-// lembrando: perímetro é a soma dos quatro lados do quadrado
-function perimetroQuadrado(lado) {
-  return lado * 4;
-}
+// Conjunto de variáveis e funções, que são chamadas de propriedades e métodos
 
-// Crie uma função que retorne o seu nome completo
-// ela deve possuir os parâmetros: nome e sobrenome
-function nomeCompleto(nome, sobrenome) {
-  return `${nome} ${sobrenome} `;
-}
+var pessoa = {
+  nome: "Luan",
+  idade: 23,
+  possuiFaculdade: true,
+};
 
-// Crie uma função que verifica se um número é par
-function isEven(numero) {
-  if (numero % 2 === 0) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-// Crie uma função que retorne o tipo de
-// dado do argumento passado nela (typeof)
-function tipoDeDado(dado) {
-  return typeof dado;
-}
-
-// addEventListener é uma função nativa do JavaScript
-// o primeiro parâmetro é o evento que ocorre e o segundo o Callback
-// utilize essa função para mostrar no console o seu nome completo
-// quando o evento 'scroll' ocorrer.
-addEventListener("scroll", function () {
-  console.log("Luan Argolo");
-});
-
-// Corrija o erro abaixo
-var totalPaises = 193;
-function precisoVisitar(paisesVisitados) {
-  return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
-}
-function jaVisitei(paisesVisitados) {
-  return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
-}
-console.log(precisoVisitar(20));
-console.log(jaVisitei(20));
+console.log(pessoa.nome); // Luan
+console.log(pessoa.possuiFaculdade); // true

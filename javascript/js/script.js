68876c459433da316bcf2b6ac3fb5692dab10467
Luan Argolo <ipsluan@icloud.com>
2022-02-04 20:47:43 -0300

    Uso de arrays

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -1,47 +1,11 @@
-// Tudo é Objeto
+// Array
 
-// Strings, Números, Boolean, Objetos e mais, possuem propriedades e métodos. Por isso são objetos.
+// É um grupo de valores geralmente relacionados. Servem para guardarmos diferentes valores em uma única variável.
 
-var nome = "Luan";
+var videoGames = ["Switch", "PS4", "Xbox"];
 
-nome.length; // 4
-nome.charAt(1); // u
-nome.replace("an", "am"); // 'Luam'
-nome; // Luan
+console.log(videoGames[0]); // Switch
+console.log(videoGames[1]); // PS4
+console.log(videoGames[2]); // Xbox
 
-var altura = 1.85;
-altura.toString(); // '1.85'
-altura.toFixed(); // 2 -> arredondamento
-
-// Por um breve momento, o número é envolvido em um Objeto (coerção), tendo acesso assim as suas propriedades e métodos
-
-// -> Elementos do DOM
-var btn = document.querySelector(".btn");
-
-btn.classList.add("azul");
-console.log(btn.innerText);
-btn.addEventListener("click", function () {
-  console.log("clicou!");
-});
-
-// Praticamente todos os efeitos com JS são feitos utilizando propriedades e métodos de objetos do DOM
-
-// # Exercícios
-
-// nomeie 3 propriedades ou métodos de Strings
-var sobrenome = "Argolo";
-// fixed
-// length
-// slice
-
-// nomeie 5 propriedades ou métodos de elementos do DOM
-var btn = document.querySelector(".btn");
-// addEventListener
-// appendChild
-// id
-// innerHTML
-// className
-
-// busque na web um objeto (método) capaz de interagir com o clip
-// clipboard é a parte do seu computador que lida com o CTRL + C (copiar)
-// https://clipboardjs.com/
+// Acesse um elemento da array utilizand [n]

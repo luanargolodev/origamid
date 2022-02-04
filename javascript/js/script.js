a4a871fbda2f2f79922dcec9620859961003a5f7
Luan Argolo <ipsluan@icloud.com>
2022-02-04 20:01:39 -0300

    Resolução de exercícios

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -9,3 +9,38 @@ console.log(menu.hasOwnProperty("width"));
 console.log(menu.hasOwnProperty("height"));
 
 // hasOwnProperty é um método de Object
+
+var dados = {
+  nome: "Luan",
+  sobrenome: "Argolo",
+  idade: 23,
+  cidade: "Ilhéus",
+};
+
+// Crie um método no objeto anterior, que mostre o seu nome completo
+dados.nomeCompleto = function () {
+  return `${this.nome} ${this.sobrenome}`;
+};
+
+// Modifique o valor da propriedade preco para 3000
+var carro = {
+  preco: 1000,
+  portas: 4,
+  marca: "Audi",
+};
+
+carro.preco = 3000;
+
+// Crie um objeto de um cachorro que represente um labrador preto com 10 anos, que late ao ver um homem
+var cachorro = {
+  raca: "Labrador",
+  cor: "preto",
+  idade: 10,
+  latir(pessoa) {
+    if (pessoa == "homem") {
+      return "Latir";
+    } else {
+      return "Nada";
+    }
+  },
+};

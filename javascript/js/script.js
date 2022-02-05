b69d2fd9151e279d46e916ca82a875583f32eb39
Luan Argolo <ipsluan@icloud.com>
2022-02-05 16:24:54 -0300

    Atribuição, Operadores Ternários e Exercícios

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -1,11 +1,27 @@
-// Operadores de Atribuição
-// Podem funcionar como formas abreviadas
+// Operador Ternário
+// Abreviação de condicionais com if e else
 
-var x = 5;
-var y = 10;
-x += y; // x = x + y = 15;
-x -= y; // x = x - y = -5
-x *= y; // x = x * y = 50
-x /= y; // x = x / y = 0.5
-x %= y; // x = x % y = 0
-x ** +y; // x = x ** y = 9765625
+var idade = 23;
+var diabetes = false;
+var podeBeber = idade >= 18 && !diabetes ? "Pode beber" : "Não pode beber";
+console.log(podeBeber);
+
+// If Abreviado
+// Não é necessário abrir e fechar as chaves ("{}") quando retornamos apenas uma linha de código
+var possuiFaculdade = false;
+if (possuiFaculdade) console.log("Possui faculdade.");
+else console.log("Não possui faculdade.");
+
+// # Exercício
+// Some 500 ao valor de scroll abaixo, atribuindo o novo valor a scroll
+var scroll = 1000;
+scroll += 500;
+console.log(scroll);
+
+// Atribua true para a variável darCredito, caso o cliente possua carro e casa e false caso o contrário
+var possuiCarro = true;
+var possuiCasa = true;
+var darCredito =
+  possuiCarro && possuiCasa ? "Dar crédito ao cliente" : "Negar crédito";
+
+console.log(darCredito);

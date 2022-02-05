2da278f8b14193cc46aa4d3c2ce301e4f5bcc621
Luan Argolo <ipsluan@icloud.com>
2022-02-05 16:55:26 -0300

    Correção dos exercícios

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -59,20 +59,20 @@ console.log(cor);
 // Var não é uma variável. Const e let não vazam no escopo
 
 // Como corrigir o erro abaixo?
+const dois = 2;
 function somarDois(x) {
-  const dois = 2;
   return x + dois;
 }
 function dividirDois(x) {
-  return x + dois;
+  return x / dois;
 }
-somarDois(4);
-dividirDois(6);
+console.log(somarDois(4));
+console.log(dividirDois(6));
 
 // O que fazer para total retornar 500?
-var numero = 50;
+const numero = 50;
 
-for (var numero = 0; numero < 10; numero++) {
+for (let numero = 0; numero < 10; numero++) {
   console.log(numero);
 }
 

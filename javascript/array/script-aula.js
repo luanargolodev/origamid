db7479b209918c8e0ed69bd678c5b3b1bede49ce
Luan Argolo <ipsluan@icloud.com>
2022-02-24 14:16:18 -0300

    Resolver exercício

--- javascript/array/script-aula.js
+++ javascript/array/script-aula.js
@@ -39,3 +39,112 @@ console.log(linguagens.slice(3));
 console.log(linguagens.slice(1, 4));
 const cloneLinguagens = linguagens.slice();
 console.log(cloneLinguagens);
+
+// [].forEach = item (valor do item da array), index = (index do valor na array)
+// array = (array original)
+carros.forEach((item) => {
+  console.log(item.toUpperCase());
+});
+
+// [].map
+const novaArray = carros.map((item) => {
+  return item.toUpperCase();
+});
+console.log(novaArray);
+
+const aulas = [
+  {
+    nome: "HTML 1",
+    min: 15,
+  },
+  {
+    nome: "HTML 2",
+    min: 10,
+  },
+  {
+    nome: "CSS 1",
+    min: 20,
+  },
+  {
+    nome: "JS 1",
+    min: 25,
+  },
+];
+
+const tempoAulas = aulas.map((aula) => {
+  return aula.min;
+});
+
+const nomeAulas = aulas.map((aula) => {
+  return aula.nome;
+});
+
+// [].reduce
+const _aulas = [10, 25, 30];
+
+const reduceAulas = _aulas.reduce((acumulador, item) => {
+  console.log(acumulador, item);
+  return acumulador + item;
+}, 0);
+console.log(reduceAulas);
+
+const numeros = [10, 3, 6, 65, 35, 9000];
+
+const maiorValor = numeros.reduce((anterior, atual) => {
+  return anterior > atual ? anterior : atual;
+}, 0);
+console.log(`Maior número: ${maiorValor}`);
+
+// [].reduceRight
+const frutas = ["Banana", "Pêra", "Mamão", "Abacaxi", "Uva"];
+const frutasRight = frutas.reduceRight((acc, fruta) => acc + " " + fruta);
+const frutasLeft = frutas.reduce((acc, fruta) => acc + " " + fruta);
+
+console.log(frutasRight);
+console.log(frutasLeft);
+
+// [].some
+const temUva = frutas.some((fruta) => {
+  return fruta === "Uva";
+});
+console.log(temUva);
+
+function maiorQue100(numero) {
+  return numero > 100;
+}
+
+const numeros_ = [6, 43, 22, 88, 101, 3];
+const temMaior = numeros_.some(maiorQue100);
+console.log(temMaior);
+
+// [].every
+const arrayCheias = frutas.every((fruta) => {
+  return fruta;
+});
+console.log(arrayCheias);
+
+const maiorQue3 = numeros_.every((x) => x > 3);
+console.log(maiorQue3);
+
+// [].find e [].findIndex
+const buscaUva = frutas.findIndex((fruta) => {
+  return fruta === "Uva";
+});
+console.log(buscaUva);
+
+const maiorQue45 = numeros_.find((x) => x > 45);
+console.log(maiorQue45);
+
+// [].filter
+const arrayLimpa = frutas.filter((fruta) => {
+  return fruta;
+});
+console.log(arrayLimpa);
+
+const maior45 = numeros_.filter((x) => x > 45);
+console.log(maior45);
+
+const maioresQue15 = aulas.filter((aula) => {
+  return aula.min > 15;
+});
+console.log(maioresQue15);
--- javascript/array/script.js
+++ javascript/array/script.js
@@ -1,34 +1,60 @@
-const comidas = ["Pizza", "Frango", "Carne", "Macarrão"];
-// Remova o primeiro valor de comidas e coloque em uma variável
-// Remova o último valor de comidas e coloque em uma variável
-// Adicione 'Arroz' ao final da array
-// Adicione 'Peixe' e 'Batata' ao início da array
-const primeiroValor = comidas.shift();
-const ultimoValor = comidas.pop();
-comidas.push("Arroz");
-comidas.unshift("Peixe", "Batata");
+// Selecione cada curso e retorne uma array
+// com objetos contendo o título, descricao,
+// aulas e horas de cada curso
+const cursos = document.querySelectorAll(".curso");
+const arrayCursos = Array.from(cursos);
+const objetosCursos = arrayCursos.map((curso) => {
+  const titulo = curso.querySelector("h1").innerText;
+  const descricao = curso.querySelector("p").innerText;
+  const aulas = curso.querySelector(".aulas").innerText;
+  const horas = curso.querySelector(".horas").innerText;
+  return {
+    titulo,
+    descricao,
+    aulas,
+    horas,
+  };
+});
+console.log(objetosCursos);
 
-const estudantes = ["Marcio", "Brenda", "Joana", "Kleber", "Julia"];
-// Arrume os estudantes em ordem alfabética
-// Inverta a ordem dos estudantes
-// Verifique se Joana faz parte dos estudantes
-// Verifique se Juliana faz parte dos estudantes
-estudantes.sort();
-estudantes.reverse();
-console.log(estudantes.includes("Joana"));
-console.log(estudantes.includes("Juliana"));
+// Retorne uma lista com os
+// números maiores que 100
+const numeros = [3, 44, 333, 23, 122, 322, 33];
+const maioresQue100 = numeros.filter((n) => n > 100);
+console.log(maioresQue100);
 
-let html = `<section>
-              <div>Sobre</div>
-              <div>Produtos</div>
-              <div>Contato</div>
-            </section>`;
-// Substitua section por ul e div com li,
-// utilizando split e join
-html = html.split("section").join("ul").split("div").join("li");
+// Verifique se Baixo faz parte
+// da lista de instrumentos e retorne true
+const instrumentos = ["Guitarra", "Baixo", "Bateria", "Teclado"];
+const possuiBaixo = instrumentos.some((i) => i === "Baixo");
+console.log(possuiBaixo);
 
-const carros = ["Ford", "Fiat", "VW", "Honda"];
-// Remova o último carro, mas antes de remover
-// salve a array original em outra variável
-const copiaCarros = carros.slice();
-carros.pop();
+// Retorne o valor total das compras
+const compras = [
+  {
+    item: "Banana",
+    preco: "R$ 4,99",
+  },
+  {
+    item: "Ovo",
+    preco: "R$ 2,99",
+  },
+  {
+    item: "Carne",
+    preco: "R$ 25,49",
+  },
+  {
+    item: "Refrigerante",
+    preco: "R$ 5,35",
+  },
+  {
+    item: "Quejo",
+    preco: "R$ 10,60",
+  },
+];
+let somaTotal = 0;
+const valorTotal = compras.reduce((acumulador, item) => {
+  const precoLimpo = +item.preco.replace("R$", "").replace(",", ".");
+  return acumulador + precoLimpo;
+}, 0);
+console.log(valorTotal);

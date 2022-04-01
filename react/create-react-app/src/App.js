1a75e5357c80238053cf92b761c028a756841e5b
Luan Argolo <ipsluan@icloud.com>
2022-04-01 15:25:55 -0300

    :sparkles: feat: exercícios

--- react/create-react-app/src/App.js
+++ react/create-react-app/src/App.js
@@ -1,46 +1,28 @@
-import React from "react";
+// Organize os produtos como mostrado no vídeo
+// Mostre apenas produtos que forem mais caros que R$ 1500
+const produtos = [
+  {
+    id: 1,
+    nome: "Smartphone",
+    preco: "R$ 2000",
+    cores: ["#29d8d5", "#252a34", "#fc3766"],
+  },
+  {
+    id: 2,
+    nome: "Notebook",
+    preco: "R$ 3000",
+    cores: ["#ffd045", "#d4394b", "#f37c59"],
+  },
+  {
+    id: 3,
+    nome: "Tablet",
+    preco: "R$ 1500",
+    cores: ["#365069", "#47c1c8", "#f95786"],
+  },
+];
 
 const App = () => {
-  const filmes = ["Before Sunrise", "Before Sunset", "Before Midnight"];
-
-  const livros = [
-    {
-      nome: "A Game of Thrones",
-      ano: 1996,
-    },
-    {
-      nome: "A Clash of Kings",
-      ano: 1998,
-    },
-    {
-      nome: "A Storm of Swords",
-      ano: 2000,
-    },
-  ];
-
-  return (
-    <>
-      <ul>
-        {filmes.map((filme) => (
-          <li key={filme}>{filme}</li>
-        ))}
-      </ul>
-
-      <ul>
-        {livros
-          // Filtrando os livros que possuem o ano maior ou igual a 1998
-          .filter((livro) => livro.ano >= 1998)
-          // Percorrendo todos os livros e desestruturando o nome e o ano da array livros
-          .map(({ nome, ano }) => (
-            // Criando uma li
-            <li key={nome}>
-              {/* Exibindo o nome do livro e o ano */}
-              {nome}, {ano}
-            </li>
-          ))}
-      </ul>
-    </>
-  );
+  return <section></section>;
 };
 
 export default App;

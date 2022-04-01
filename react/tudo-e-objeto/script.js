b5a721a7236f5f57b8ff8e7a701700dfcfaea8e4
Luan Argolo <ipsluan@icloud.com>
2022-04-01 02:54:07 -0300

    :sparkles: feat: primeiro component em react

--- react/tudo-e-objeto/script.js
+++ react/tudo-e-objeto/script.js
@@ -1,3 +1,10 @@
+import quadrado from "./quadrado.js";
+import numeroAleatorio from "./numeroAleatorio.js";
+
+console.log(quadrado.areaQuadrado(5));
+console.log(quadrado.perimetroQuadrado(5));
+console.log(numeroAleatorio());
+
 // Desestruturando um objeto
 function handleMouse({ clientX, clientY }) {
   console.log(clientX, clientY);
@@ -41,3 +48,31 @@ const carro = {
 };
 carro.ano = 2020;
 console.log(carro);
+
+const precos = [
+  "Crédito",
+  "R$ 200",
+  "R$ 400",
+  "Contas Pagar",
+  "R$ 300",
+  "R$ 400",
+  "Meus Dados",
+];
+const precosFilter = precos.filter((p) => p.includes("R$"));
+const precosNumeros = precosFilter.map((p) => parseInt(p.replace("R$", "")));
+console.log(precosNumeros);
+
+const grupoA = 100;
+const grupoB = 200;
+if (grupoA > grupoB) {
+  console.log("Grupo A ganhou");
+} else {
+  console.log("Grupo B ganhou");
+}
+
+const vencedor = grupoA > grupoB ? "Grupo A ganhou" : "Grupo B ganhou";
+console.log(vencedor);
+
+const active = true;
+const button = active && "Botão está ativo";
+console.log(button);

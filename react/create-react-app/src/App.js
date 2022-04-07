8710a0709fa943f556e19f58a6d1f313941a87bb
Luan Argolo <ipsluan@icloud.com>
2022-04-07 00:39:38 -0300

    :sparkles: feat: exemplo de página com uso de API

--- react/create-react-app/src/App.js
+++ react/create-react-app/src/App.js
@@ -1,16 +1,33 @@
 import React, { useState } from "react";
-
-import Modal from "./Modal";
-import ButtonModal from "./ButtonModal";
+import Produto from "./Produto";
 
 const App = () => {
-  const [modal, setModal] = useState(false);
+  const [dados, setDados] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  async function handleClick(event) {
+    setLoading(true);
+    const response = await fetch(
+      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`
+    );
+    const json = await response.json();
+    setDados(json);
+    setLoading(false);
+  }
 
   return (
     <div>
-      <div>{modal ? "Modal aberto" : "Modal fechado"}</div>
-      <Modal modal={modal} setModal={setModal} />
-      <ButtonModal setModal={setModal} />
+      <button style={{ margin: ".5rem" }} onClick={handleClick}>
+        Notebook
+      </button>
+      <button style={{ margin: ".5rem" }} onClick={handleClick}>
+        Smartphone
+      </button>
+      <button style={{ margin: ".5rem" }} onClick={handleClick}>
+        Tablet
+      </button>
+      {loading && <p>Carregando...</p>}
+      {!loading && dados && <Produto dados={dados} />}
     </div>
   );
 };

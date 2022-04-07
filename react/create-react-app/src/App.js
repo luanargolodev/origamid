242bfbe575bdda927650da2e31b447f7e8022d64
Luan Argolo <ipsluan@icloud.com>
2022-04-07 00:59:10 -0300

    :sparkles: feat: introdução ao useEffect

--- react/create-react-app/src/App.js
+++ react/create-react-app/src/App.js
@@ -2,32 +2,12 @@ import React, { useState } from "react";
 import Produto from "./Produto";
 
 const App = () => {
-  const [dados, setDados] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  async function handleClick(event) {
-    setLoading(true);
-    const response = await fetch(
-      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`
-    );
-    const json = await response.json();
-    setDados(json);
-    setLoading(false);
-  }
+  const [ativo, setAtivo] = useState(false);
 
   return (
     <div>
-      <button style={{ margin: ".5rem" }} onClick={handleClick}>
-        Notebook
-      </button>
-      <button style={{ margin: ".5rem" }} onClick={handleClick}>
-        Smartphone
-      </button>
-      <button style={{ margin: ".5rem" }} onClick={handleClick}>
-        Tablet
-      </button>
-      {loading && <p>Carregando...</p>}
-      {!loading && dados && <Produto dados={dados} />}
+      {ativo && <Produto />}
+      <button onClick={() => setAtivo(!ativo)}>Ativar</button>
     </div>
   );
 };

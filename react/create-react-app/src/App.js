17210c16d4f1271152a0eb227f4e49ea867f7a05
Luan Argolo <ipsluan@icloud.com>
2022-04-12 13:51:40 -0300

    :sparkles: feat: criar formulário com cadastro fetch POST api Ranek

--- react/create-react-app/src/App.js
+++ react/create-react-app/src/App.js
@@ -1,19 +1,36 @@
 import React, { useState } from "react";
 
 const App = () => {
+  const [response, setResponse] = useState(null);
+
   const [form, setForm] = useState({
     nome: "",
     email: "",
+    senha: "",
+    cep: "",
+    rua: "",
+    bairro: "",
+    cidade: "",
+    estado: "",
   });
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    alert("Seu nome é " + form.nome + " e seu e-mail é " + form.email);
-  }
 
   function handleChange({ target }) {
     const { id, value } = target;
     setForm({ ...form, [id]: value });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    fetch("https://ranekapi.origamid.dev/json/api/usuario", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(form),
+    }).then((response) => {
+      setResponse(response);
+    });
   }
 
   return (
@@ -23,12 +40,52 @@ const App = () => {
 
       <label htmlFor="email">Email</label>
       <input
-        type="email"
+        type="text"
         id="email"
         value={form.email}
         onChange={handleChange}
       />
+
+      <label htmlFor="senha">Senha</label>
+      <input
+        type="password"
+        id="senha"
+        value={form.senha}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="cep">CEP</label>
+      <input type="text" id="cep" value={form.cep} onChange={handleChange} />
+
+      <label htmlFor="rua">Rua</label>
+      <input type="text" id="rua" value={form.rua} onChange={handleChange} />
+
+      <label htmlFor="bairro">Bairro</label>
+      <input
+        type="text"
+        id="bairro"
+        value={form.bairro}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="cidade">Cidade</label>
+      <input
+        type="text"
+        id="cidade"
+        value={form.cidade}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="estado">Estado</label>
+      <input
+        type="text"
+        id="estado"
+        value={form.estado}
+        onChange={handleChange}
+      />
+
       <button>Enviar</button>
+      {response && response.ok && <p>Formulário enviado com sucesso!</p>}
     </form>
   );
 };

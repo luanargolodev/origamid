47dade74043a45c32a05890ed41c52fde9d3031c
Luan Argolo <ipsluan@icloud.com>
2022-04-12 17:52:09 -0300

    :sparkles: feat: Select em component

--- react/create-react-app/src/App.js
+++ react/create-react-app/src/App.js
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
 import Input from "./components/Input";
+import Select from "./components/Select";
 
 const App = () => {
   const [nome, setNome] = useState("");
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [produto, setProduto] = useState("");
 
   return (
-    <div>
+    <form>
+      <Select
+        options={["Celular", "Tablet"]}
+        value={produto}
+        setValue={setProduto}
+      />
       <Input id="nome" label="Nome" value={nome} setValue={setNome} />
       <Input id="email" label="Email" value={email} setValue={setEmail} />
-      <Input
-        id="password"
-        label="Senha"
-        value={password}
-        setValue={setPassword}
-      />
-    </div>
+    </form>
   );
 };
 

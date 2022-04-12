b286e950a2d47809d63ba26be10f3ed4e744065e
Luan Argolo <ipsluan@icloud.com>
2022-04-12 13:17:18 -0300

    :sparkles: feat: formulário com padrão alternativo

--- react/create-react-app/src/App.js
+++ react/create-react-app/src/App.js
@@ -1,30 +1,32 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [nome, setNome] = useState("");
-  const [email, setEmail] = useState("");
+  const [form, setForm] = useState({
+    nome: "",
+    email: "",
+  });
 
   function handleSubmit(event) {
     event.preventDefault();
-    alert("Seu nome é " + nome + " e seu e-mail é " + email);
+    alert("Seu nome é " + form.nome + " e seu e-mail é " + form.email);
+  }
+
+  function handleChange({ target }) {
+    const { id, value } = target;
+    setForm({ ...form, [id]: value });
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="nome">Nome</label>
-      <input
-        type="text"
-        id="nome"
-        value={nome}
-        onChange={(e) => setNome(e.target.value)}
-      />
+      <input type="text" id="nome" value={form.nome} onChange={handleChange} />
 
       <label htmlFor="email">Email</label>
       <input
         type="email"
         id="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        value={form.email}
+        onChange={handleChange}
       />
       <button>Enviar</button>
     </form>

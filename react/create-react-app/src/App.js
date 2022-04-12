43eb17ee480c420767e98997b1b7c12d9c2bad24
Luan Argolo <ipsluan@icloud.com>
2022-04-12 16:32:52 -0300

    :sparkles: feat: uso de checkbox reativo

--- react/create-react-app/src/App.js
+++ react/create-react-app/src/App.js
@@ -1,88 +1,34 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [textarea, setTextarea] = useState("");
-  const [select, setSelect] = useState("");
-  const [produto, setProduto] = useState("");
-  const [form, setForm] = useState({
-    nome: "",
-    email: "",
-    mensagem: "",
-  });
+  const [cores, setCores] = useState([""]);
 
   function handleChange({ target }) {
-    const { id, value } = target;
-    setForm({ ...form, [id]: value });
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-  }
-
-  function handleChangeRadio({ target }) {
-    setProduto(target.value);
+    if (target.checked) setCores([...cores, target.value]);
+    else setCores(cores.filter((cor) => cor !== target.value));
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="nome">Nome</label>
-      <input type="text" id="nome" value={form.nome} onChange={handleChange} />
-
-      <label htmlFor="email">Email</label>
-      <input
-        type="text"
-        id="email"
-        value={form.email}
-        onChange={handleChange}
-      />
-
-      <label htmlFor="produtos">Produtos</label>
-      <select
-        name="produtos"
-        id="produtos"
-        value={select}
-        onChange={({ target }) => setSelect(target.value)}
-      >
-        <option disabled value="">
-          Selecione
-        </option>
-        <option value="notebook">Notebook</option>
-        <option value="smartphone">Smartphone</option>
-        <option value="tablet">Tablet</option>
-      </select>
-      {select}
-
-      <label>
+    <form>
+      <label htmlFor="termos">
         <input
-          type="radio"
-          name="produto"
-          value="smartphone"
-          checked={produto === "smartphone"}
-          onChange={handleChangeRadio}
+          type="checkbox"
+          value="azul"
+          checked={cores.includes("azul")}
+          onChange={handleChange}
         />
-        Smartphone
-      </label>
-      <label>
-        <input
-          type="radio"
-          name="produto"
-          value="notebook"
-          checked={produto === "notebook"}
-          onChange={handleChangeRadio}
-        />
-        Notebook
+        Azul
       </label>
 
-      <label htmlFor="mensagem">Mensagem</label>
-      <textarea
-        id="mensagem"
-        value={textarea}
-        onChange={({ target }) => setTextarea(target.value)}
-        rows="5"
-      />
-      {textarea}
-
-      <button>Enviar</button>
+      <label htmlFor="termos">
+        <input
+          type="checkbox"
+          value="vermelho"
+          checked={cores.includes("vermelho")}
+          onChange={handleChange}
+        />
+        Vermelho
+      </label>
     </form>
   );
 };

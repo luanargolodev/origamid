88038fe645c0798fecd6dec42a940e12b822b758
Luan Argolo <ipsluan@icloud.com>
2022-04-12 17:59:10 -0300

    :sparkles: feat: Radio em component

--- react/create-react-app/src/App.js
+++ react/create-react-app/src/App.js
@@ -1,19 +1,33 @@
 import React, { useState } from "react";
 import Input from "./components/Input";
+import Radio from "./components/Radio";
 import Select from "./components/Select";
 
 const App = () => {
   const [nome, setNome] = useState("");
   const [email, setEmail] = useState("");
   const [produto, setProduto] = useState("");
+  const [cor, setCor] = useState("");
+  const [frutas, setFrutas] = useState("");
 
   return (
     <form>
+      <h2>Cores</h2>
+      <Radio options={["Azul", "Vermelho"]} value={cor} setValue={setCor} />
+
+      <h2>Frutas</h2>
+      <Radio
+        options={["Limão", "Laranja", "Uva"]}
+        value={frutas}
+        setValue={setFrutas}
+      />
+
       <Select
         options={["Celular", "Tablet"]}
         value={produto}
         setValue={setProduto}
       />
+
       <Input id="nome" label="Nome" value={nome} setValue={setNome} />
       <Input id="email" label="Email" value={email} setValue={setEmail} />
     </form>

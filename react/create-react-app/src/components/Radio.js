e3c617fa2c14f0342264b48b5d21b8d6d5b4f485
Luan Argolo <ipsluan@icloud.com>
2022-04-14 10:10:25 -0300

    :sparkles: feat: desafio de formulários

--- react/create-react-app/src/components/Radio.js
+++ react/create-react-app/src/components/Radio.js
@@ -1,21 +1,34 @@
 import React from "react";
 
-const Radio = ({ options, value, setValue, ...props }) => {
+const Radio = ({ pergunta, options, id, value, onChange, active }) => {
+  if (active === false) return null;
+
   return (
-    <>
+    <fieldset
+      style={{
+        padding: "2rem",
+        marginBottom: "1rem",
+        border: "2px solid #eee",
+      }}
+    >
+      <legend style={{ fontWeight: "bold" }}>{pergunta}</legend>
       {options.map((option) => (
-        <label key={option}>
+        <label
+          key={option}
+          style={{ marginBottom: "1rem", fontFamily: "monospace" }}
+        >
           <input
             type="radio"
-            value={option}
             checked={value === option}
-            onChange={({ target }) => setValue(target.value)}
-            {...props}
+            name={id}
+            id={id}
+            value={option}
+            onChange={onChange}
           />
           {option}
         </label>
       ))}
-    </>
+    </fieldset>
   );
 };
 

d75080ecf1a4fcfc1edf501377db8c8bc7d7e62a
Luan Argolo <ipsluan@icloud.com>
2022-04-26 12:17:45 -0300

    :sparkles: feat: mudar Head

--- react/create-react-app/src/pages/Home.js
+++ react/create-react-app/src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Head from '../components/Head';
 
 const Home = () => {
   return (
-    <h1>
-      Home
+    <div>
+      <Head title="Home" description="Home do site" />
+      <h1>Home</h1>
       <p>Essa é a home</p>
-    </h1>
+    </div>
   );
 };
 

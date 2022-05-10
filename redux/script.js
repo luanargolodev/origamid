1edbc384edff79c5a3c6397e5d7993dbd587dcf5
Luan Argolo <ipsluan@icloud.com>
2022-05-10 11:08:43 -0300

    :sparkles: feat: uso de currying

---
+++ redux/script.js
@@ -0,0 +1,16 @@
+const li = Array.from(document.querySelectorAll('li'));
+
+const getElementAttr = (key) => {
+  return function (el) {
+    return el.getAttribute(key);
+  };
+};
+
+const getAttrDataSlide = getElementAttr('data-slide');
+const getAttrId = getElementAttr('id');
+
+const dataSlideList = li.map(getAttrDataSlide);
+const idList = li.map(getAttrId);
+
+console.log(dataSlideList);
+console.log(idList);
